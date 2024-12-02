c7ed7f82b30162dc7636a06d44b4b43ee616dc8f
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-12-02 08:57:26 +0100

    2024: Add dayxx template

---
+++ 2024/source/dayxx/part1.d
@@ -0,0 +1,12 @@
+module dayxx.part1;
+
+import std;
+import std.format : formattedRead;
+import std.stdio : stdin, writeln;
+
+void solve()
+{
+    stdin.byLine
+        .front
+        .writeln;
+}
---
+++ 2024/source/dayxx/part2.d
@@ -0,0 +1,12 @@
+module dayxx.part2;
+
+import std;
+import std.format : formattedRead;
+import std.stdio : stdin, writeln;
+
+void solve()
+{
+    stdin.byLine
+        .front
+        .writeln;
+}
