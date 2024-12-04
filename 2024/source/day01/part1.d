5c001092001a7d8f770ce2fc4b9512ca014f17d6
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-12-04 14:59:27 +0100

    2024: Redo day01 part1

--- 2024/source/day01/part1.d
+++ 2024/source/day01/part1.d
@@ -1,19 +1,30 @@
 module day01.part1;
 
-import std.algorithm : fold, map, sort, sum;
-import std.array : array, split;
-import std.conv : to;
-import std.range : transposed;
+import std.algorithm : map, sum;
+import std.array : array;
+import std.container.binaryheap : BinaryHeap;
+import std.format : formattedRead;
+import std.math : abs;
+import std.range : zip;
 import std.stdio : stdin, writeln;
 
 void solve()
 {
-    stdin.byLine
-        .map!(line => line.split.map!(to!int).array).array
-        .transposed
-        .map!(r => r.array.sort).array
-        .transposed
-        .map!(fold!"abs(a-b)")
+    int[][2] lists;
+
+    foreach (line; stdin.byLine)
+    {
+        int[2] ids;
+        line.formattedRead!"%d   %d"(ids[0], ids[1]);
+        lists[0] ~= ids[0];
+        lists[1] ~= ids[1];
+    }
+
+    alias Heap = BinaryHeap!(int[]);
+    Heap[2] heaps = lists[].map!Heap.array;
+
+    zip(heaps[0], heaps[1])
+        .map!(tup => abs(tup[0] - tup[1]))
         .sum
         .writeln;
 }
