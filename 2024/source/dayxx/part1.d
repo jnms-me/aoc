65133ffa64ea036705f92f4fccd0993c5566d79a
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-12-14 21:18:00 +0100

    2024: Remove slow import std; from template

--- 2024/source/dayxx/part1.d
+++ 2024/source/dayxx/part1.d
@@ -1,12 +1,5 @@
 module dayxx.part1;
 
-import std;
-import std.format : formattedRead;
-import std.stdio : stdin, writeln;
-
 void solve()
 {
-    stdin.byLine
-        .front
-        .writeln;
 }
--- 2024/source/dayxx/part2.d
+++ 2024/source/dayxx/part2.d
@@ -1,12 +1,5 @@
 module dayxx.part2;
 
-import std;
-import std.format : formattedRead;
-import std.stdio : stdin, writeln;
-
 void solve()
 {
-    stdin.byLine
-        .front
-        .writeln;
 }
